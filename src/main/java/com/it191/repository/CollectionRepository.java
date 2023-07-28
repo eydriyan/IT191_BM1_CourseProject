77b9bf5d751c54b026885e0fa5495407cc76eee1
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-07-28 09:48:19 +0800

    Remove Multi-User

--- src/main/java/com/it191/repository/CollectionRepository.java
+++ src/main/java/com/it191/repository/CollectionRepository.java
@@ -8,14 +8,12 @@ import java.util.ArrayList;
 
 import com.it191.model.CollectionModel;
 import com.it191.model.SongModel;
-import com.it191.model.UserModel;
 
 public class CollectionRepository extends RepositoryBase {
-public void createNewCollection(UserModel userModel, CollectionModel collectionModel) {
+public void createNewCollection(CollectionModel collectionModel) {
         try (Connection connection = getSqlConnection()) {
-            String insertQuery = "INSERT INTO Collection (user_id, title) VALUES (?, ?)";
+            String insertQuery = "INSERT INTO Collection (title) VALUES (?)";
             PreparedStatement statement = connection.prepareStatement(insertQuery);
-            statement.setInt(1, userModel.getUserId());
             statement.setString(2, collectionModel.getTitle());
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -23,12 +21,11 @@ public void createNewCollection(UserModel userModel, CollectionModel collectionM
         }
     }
 
-    public ArrayList<CollectionModel> getAllCollections(UserModel userModel) {
+    public ArrayList<CollectionModel> getAllCollections() {
         ArrayList<CollectionModel> collections = new ArrayList<>();
         try (Connection connection = getSqlConnection()) {
-            String selectQuery = "SELECT * FROM Collection WHERE user_id = ?";
+            String selectQuery = "SELECT * FROM Collection";
             PreparedStatement statement = connection.prepareStatement(selectQuery);
-            statement.setInt(1, userModel.getUserId());
             ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
@@ -43,32 +40,30 @@ public void createNewCollection(UserModel userModel, CollectionModel collectionM
         return collections;
     }
 
-    public void updateCollectionInfo(UserModel userModel, CollectionModel collectionModel) {
+    public void updateCollectionInfo(CollectionModel collectionModel) {
         try (Connection connection = getSqlConnection()) {
-            String updateQuery = "UPDATE Collection SET title = ? WHERE collection_id = ? AND user_id = ?";
+            String updateQuery = "UPDATE Collection SET title = ? WHERE collection_id = ?";
             PreparedStatement statement = connection.prepareStatement(updateQuery);
             statement.setString(1, collectionModel.getTitle());
             statement.setInt(2, collectionModel.getCollectionId());
-            statement.setInt(3, userModel.getUserId());
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void deleteCollectionInfo(UserModel userModel, CollectionModel collectionModel) {
+    public void deleteCollectionInfo(CollectionModel collectionModel) {
         try (Connection connection = getSqlConnection()) {
-            String deleteQuery = "DELETE FROM Collection WHERE collection_id = ? AND user_id = ?";
+            String deleteQuery = "DELETE FROM Collection WHERE collection_id = ?";
             PreparedStatement statement = connection.prepareStatement(deleteQuery);
             statement.setInt(1, collectionModel.getCollectionId());
-            statement.setInt(2, userModel.getUserId());
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void addSongToCollection(UserModel userModel, SongModel songModel, CollectionModel collectionModel) {
+    public void addSongToCollection(SongModel songModel, CollectionModel collectionModel) {
         try (Connection connection = getSqlConnection()) {
             String insertQuery = "INSERT INTO CollectionSong (collection_id, song_id) VALUES (?, ?)";
             PreparedStatement statement = connection.prepareStatement(insertQuery);
@@ -80,7 +75,7 @@ public void createNewCollection(UserModel userModel, CollectionModel collectionM
         }
     }
 
-    public void removeSongFromCollection(UserModel userModel, SongModel songModel, CollectionModel collectionModel) {
+    public void removeSongFromCollection(SongModel songModel, CollectionModel collectionModel) {
         try (Connection connection = getSqlConnection()) {
             String deleteQuery = "DELETE FROM CollectionSong WHERE collection_id = ? AND song_id = ?";
             PreparedStatement statement = connection.prepareStatement(deleteQuery);
