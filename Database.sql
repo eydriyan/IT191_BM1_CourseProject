69fc0d1d8754047111e027b6f5ee2003aa84cab2
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-07-30 07:05:13 +0800

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -11,30 +11,20 @@ DELETE FROM CollectionSong;
 DELETE FROM Colllection;
 DELETE FROM Song;
 DELETE FROM Genre;
-DELETE FROM User;
 
 -- Drop Tables
 DROP TABLE CollectionSong;
 DROP TABLE Collection;
 DROP TABLE Song;
 DROP TABLE Genre;
-DROP TABLE User;
 
 SELECT * FROM CollectionSong;
 SELECT * FROM Collection;
 SELECT * FROM Song;
 SELECT * FROM Genre;
-SELECT * FROM User;
 
 
 -- Create Tables
-CREATE TABLE User (
-	user_id INT PRIMARY KEY auto_increment,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    dob DATE NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
 
 CREATE TABLE Genre (
 	genre_id INT PRIMARY KEY auto_increment,
@@ -43,14 +33,13 @@ CREATE TABLE Genre (
 
 CREATE TABLE Song (
 	song_id INT PRIMARY KEY auto_increment,
-    artist_id VARCHAR(100),
+    artist VARCHAR(100),
     title VARCHAR(100),
     genre_id INT,
     lyrics TEXT,
     duration INT,
     song_path VARCHAR(100),
     img_path VARCHAR(100),
-    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id) ON DELETE CASCADE,
     FOREIGN KEY (genre_id) REFERENCES Genre(genre_id) ON DELETE CASCADE
 );
 
