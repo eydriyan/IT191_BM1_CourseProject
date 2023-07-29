37058fecd49a05521af24bb129f9d12ca63b934f
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-07-30 07:05:35 +0800

    add setLyrics to LyricsPanel

--- src/main/java/com/it191/view/LyricsPanel.java
+++ src/main/java/com/it191/view/LyricsPanel.java
@@ -1,22 +1,29 @@
 package com.it191.view;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class LyricsPanel extends JScrollPane {
     
-    private javax.swing.JTextArea jTextArea1;
+    private JTextArea lyricsArea;
 
     public LyricsPanel(){
-        jTextArea1 = new javax.swing.JTextArea();
+        lyricsArea = new javax.swing.JTextArea();
 
-        jTextArea1.setBackground(new java.awt.Color(51, 51, 51));
-        jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("STXihei", 1, 20)); // NOI18N
-        jTextArea1.setForeground(new java.awt.Color(255, 255, 255));
-        jTextArea1.setLineWrap(true);
-        jTextArea1.setRows(5);
-        jTextArea1.setBorder(null);
-        this.setViewportView(jTextArea1);
+        lyricsArea.setBackground(new Color(51, 51, 51));
+        lyricsArea.setColumns(20);
+        lyricsArea.setFont(new Font("STXihei", 1, 20)); // NOI18N
+        lyricsArea.setForeground(new Color(255, 255, 255));
+        lyricsArea.setLineWrap(true);
+        lyricsArea.setRows(5);
+        lyricsArea.setBorder(null);
+        this.setViewportView(lyricsArea);
+    }
 
+    public void setLyrics(String lyricsString) {
+        this.lyricsArea.setText(lyricsString);
     }
 }
