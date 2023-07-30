5c687461db2e583225a156553a115e02645a9c3b
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-07-30 14:13:56 +0800

    Remove Genre

--- src/main/java/com/it191/repository/SongRepository.java
+++ src/main/java/com/it191/repository/SongRepository.java
@@ -11,12 +11,29 @@ import com.it191.model.SongModel;
 
 public class SongRepository extends RepositoryBase {
 
+    public ArrayList<SongModel> getAllSongs() {
+        ArrayList<SongModel> songs = new ArrayList<>();
+        try (Connection connection = getSqlConnection()) {
+            String selectQuery = "SELECT Song.song_id, Song.artist, Song.title, Song.lyrics, Song.duration, Song.song_path, Song.img_path " +
+                                 "FROM Song";
+            PreparedStatement statement = connection.prepareStatement(selectQuery);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                SongModel song = mapResultSetToSongModel(resultSet);
+                songs.add(song);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return songs;
+    }
+
     public ArrayList<SongModel> getAllSongsFromArtist(String artist) {
         ArrayList<SongModel> songs = new ArrayList<>();
         try (Connection connection = getSqlConnection()) {
-            String selectQuery = "SELECT Song.song_id, Song.artist, Song.title, Song.lyrics, Song.duration, Song.song_path, Song.img_path, Genre.name AS genre_name " +
+            String selectQuery = "SELECT Song.song_id, Song.artist, Song.title, Song.lyrics, Song.duration, Song.song_path, Song.img_path " +
                                  "FROM Song " +
-                                 "INNER JOIN Genre ON Song.genre_id = Genre.genre_id " +
                                  "WHERE Song.artist = ?";
 
             PreparedStatement statement = connection.prepareStatement(selectQuery);
@@ -29,7 +46,6 @@ public class SongRepository extends RepositoryBase {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            // Handle the exception appropriately
         }
         return songs;
     }
@@ -37,9 +53,8 @@ public class SongRepository extends RepositoryBase {
     public ArrayList<SongModel> getAllSongsFromCollection(CollectionModel collectionModel) {
         ArrayList<SongModel> songs = new ArrayList<>();
         try (Connection connection = getSqlConnection()) {
-            String selectQuery = "SELECT Song.song_id, Song.artist, Song.title, Song.lyrics, Song.duration, Song.song_path, Song.img_path, Genre.name AS genre_name " +
+            String selectQuery = "SELECT Song.song_id, Song.artist, Song.title, Song.lyrics, Song.duration, Song.song_path, Song.img_path " +
                                  "FROM Song " +
-                                 "INNER JOIN Genre ON Song.genre_id = Genre.genre_id " +
                                  "INNER JOIN CollectionSong ON Song.song_id = CollectionSong.song_id " +
                                  "WHERE CollectionSong.collection_id = ?";
             PreparedStatement statement = connection.prepareStatement(selectQuery);
@@ -61,7 +76,6 @@ public class SongRepository extends RepositoryBase {
         song.setSongId(resultSet.getInt("song_id"));
         song.setArtist(resultSet.getString("artist"));
         song.setTitle(resultSet.getString("title"));
-        song.setGenre(resultSet.getString("genre_name"));
         song.setLyrics(resultSet.getString("lyrics"));
         song.setDuration(resultSet.getInt("duration"));
         song.setSongPath(resultSet.getString("song_path"));
