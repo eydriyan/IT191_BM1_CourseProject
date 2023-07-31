e55825ee917fd4d178c02125ee1f83189d6ace06
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-07-31 18:26:14 +0800

    Make text centered

--- src/main/java/com/it191/view/LyricsPanel.java
+++ src/main/java/com/it191/view/LyricsPanel.java
@@ -1,29 +1,45 @@
 package com.it191.view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
-
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
-public class LyricsPanel extends JScrollPane {
+public class LyricsPanel extends JPanel {
     
-    private JTextArea lyricsArea;
+    private JTextPane lyricsPane;
 
     public LyricsPanel(){
-        lyricsArea = new javax.swing.JTextArea();
+        JScrollPane scrollPane = new JScrollPane();
 
-        lyricsArea.setBackground(new Color(51, 51, 51));
-        lyricsArea.setColumns(20);
-        lyricsArea.setFont(new Font("STXihei", 1, 20)); // NOI18N
-        lyricsArea.setForeground(new Color(255, 255, 255));
-        lyricsArea.setLineWrap(true);
-        lyricsArea.setRows(5);
-        lyricsArea.setBorder(null);
-        this.setViewportView(lyricsArea);
+        lyricsPane = new JTextPane();
+        lyricsPane.setEditable(false);
+        lyricsPane.setBackground(new Color(51, 51, 51));
+        lyricsPane.setFont(new Font("STXihei", Font.PLAIN, 20));
+        lyricsPane.setForeground(new Color(255, 255, 255));
+
+        // Create a StyledDocument and set it as the StyledDocument for the JTextPane
+        StyledDocument doc = lyricsPane.getStyledDocument();
+
+        // Set the paragraph alignment to center
+        SimpleAttributeSet centerAlignment = new SimpleAttributeSet();
+        StyleConstants.setAlignment(centerAlignment, StyleConstants.ALIGN_CENTER);
+
+        // Set the center alignment style for the document
+        doc.setParagraphAttributes(0, doc.getLength(), centerAlignment, false);
+
+        scrollPane.setViewportView(lyricsPane);
+
+        this.setLayout(new BorderLayout());
+        this.add(scrollPane, BorderLayout.CENTER);
     }
 
-    public void setLyrics(String lyricsString) {
-        this.lyricsArea.setText(lyricsString);
+    public void setLyrics(String lyricsString) {            
+        this.lyricsPane.setText(lyricsString);
     }
 }
